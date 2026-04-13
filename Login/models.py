dcdb79b197599d908dc73d220b823e5a56f0d00e
PauloJacko <paulo.millanh@gmail.com>
2026-04-13 07:20:59 -0400

    Creado ORM de Django para la tabla de usuarios

--- Login/models.py
+++ Login/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=150)
+    rut = models.CharField(max_length=12, unique=True)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    fecha_nacimiento = models.DateField()
+    direccion = models.CharField(max_length=255, blank=True, null=True)
+
+    creado = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.rut}"
